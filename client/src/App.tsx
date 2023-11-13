1e9c48950974f46b477ea40d5a32ef6313840be3
kostiantyn.kashyn <kostiantyn.kashyn@gmail.com>
2023-11-13 12:04:06 +0200

    fix 1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{FC, useEffect} from 'react';
+import LoginForm from "./components/LoginForm/LoginForm";
+import {ThemeProvider} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {checkAuth} from "./store/reducers/ActionCreators";
+import {Navigate, Route, Routes} from "react-router-dom";
+import Header from "./components/header/header";
+import ItemsList from "./components/ItemsList/ItemsList";
+import Item from "./components/Item/Item";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const {user,isAuth, isLoading} = useAppSelector(state => state.userReducer)
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            dispatch(checkAuth());
+        }
+    }, []);
+    if (isLoading){
+        return (
+            <div>Loading</div>
+        )
+    }
+    if (!isAuth){
+        return (
+            <>
+                <Header isAuth={isAuth} email={user.email}></Header>
+                <LoginForm/>
+            </>
+        )
+    }
+    return (
+        <ThemeProvider>
+                <Header isAuth={isAuth} email={user.email}></Header>
+                <Routes>
+                    <Route path='/' element={
+                        <ItemsList></ItemsList>
+                    } />
+                    <Route path='/item' element={
+                        <Item></Item>
+                    } />
+                    <Route path='*' element={<Navigate to='/' />} />
+                </Routes>
+        </ThemeProvider>
+
+    );
+};
 
 export default App;
